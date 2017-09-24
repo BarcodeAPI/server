cf8954fa7d98d32570fd875d3fb273cb725e6a0f
Matthew Clark <matt@mclarkdev.com>
2017-09-24 14:11:30 -0400

    Better jetty impl.

--- src/main/java/org/barcodeapi/server/BarcodeServer.java
+++ src/main/java/org/barcodeapi/server/BarcodeServer.java
@@ -22,69 +22,57 @@ import org.xml.sax.SAXException;
 
 public class BarcodeServer extends AbstractHandler {
 
+	final int dpi = 100;
+
+	Code128Bean barcode128Bean;
+
 	public BarcodeServer() throws ConfigurationException, SAXException, IOException, BarcodeException {
 
-	}
-
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-
-		response.addHeader("Server", "barcodeapi.org");
-
-		String[] requestParts = target.split("\\/");
-
-		if (requestParts.length == 0) {
-
-			// TODO display home page
-		}
-
-		String data = requestParts[2];
-
-		switch (requestParts[1]) {
-
-		case "128":
-		case "matrix":
-		case "qr":
-			render(data);
-			response.setStatus(HttpServletResponse.SC_OK);
-			baseRequest.setHandled(true);
-			break;
-
-		default:
-			response.setContentType("text/html;charset=utf-8");
-			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			response.getWriter().println("Barcode type unavailalbe.");
-			baseRequest.setHandled(false);
-			break;
-		}
-
-	}
-
-	public void render(String data) throws IOException {
-
-		System.out.print("Rendering: " + data);
-
-		Code128Bean barcode128Bean = new Code128Bean();
-
+		barcode128Bean = new Code128Bean();
 		barcode128Bean.setCodeset(Code128Constants.CODESET_B);
-		final int dpi = 100;
 
 		// Configure the barcode generator
 		// adjust barcode width here
 		barcode128Bean.setModuleWidth(UnitConv.in2mm(5.0f / dpi));
 		barcode128Bean.doQuietZone(false);
+	}
+
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		String data = target.substring(1, target.length());
+
+		response.addHeader("Server", "barcodeapi.org");
+
+		long start = System.currentTimeMillis();
+		render(data);
+		long time = System.currentTimeMillis() - start;
+
+		response.setStatus(HttpServletResponse.SC_OK);
+		baseRequest.setHandled(true);
+		response.getOutputStream().println("Rendered [ " + data + " ] in [ " + time + "ms ]");
+	}
+
+	public void render(String data) throws IOException {
+
+		System.out.println("Rendering: " + data);
+
+		String fileName = data.replace(File.separatorChar, '-');
+		fileName = "128" + File.separator + fileName;
 
 		// Open output file
-		File outputFile = new File("cache" + File.separator + data + ".png");
+		File outputFile = new File("cache" + File.separator + fileName + ".png");
 		OutputStream out = new FileOutputStream(outputFile);
 		try {
-			BitmapCanvasProvider canvasProvider = new BitmapCanvasProvider(out, "image/x-png", dpi,
-					BufferedImage.TYPE_BYTE_BINARY, false, 0);
+
+			BitmapCanvasProvider canvasProvider = new BitmapCanvasProvider(//
+					out, "image/x-png", dpi, BufferedImage.TYPE_BYTE_BINARY, false, 0);
 
 			barcode128Bean.generateBarcode(canvasProvider, data);
 
 			canvasProvider.finish();
 		} finally {
+
 			out.close();
 		}
 	}
--- src/main/java/org/barcodeapi/server/Launcher.java
+++ src/main/java/org/barcodeapi/server/Launcher.java
@@ -1,15 +1,41 @@
 package org.barcodeapi.server;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
 
 public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 
-		Server server = new Server(8080);
-		server.setHandler(new BarcodeServer());
+		int serverPort = 8080;
+		try {
 
+			serverPort = Integer.parseInt(args[0]);
+		} catch (IndexOutOfBoundsException e) {
+
+		} catch (NumberFormatException e) {
+
+			System.err.println("Invalid port argument.");
+			System.exit(1);
+		}
+
+		Server server = new Server(serverPort);
+
+		HandlerCollection handlers = new HandlerCollection();
+
+		// setup code
+		ContextHandler _128Context = new ContextHandler();
+		_128Context.setContextPath("/128");
+		_128Context.setHandler(new BarcodeServer());
+		handlers.addHandler(_128Context);
+
+		// add handlers
+		server.setHandler(handlers);
+
+		// start server
 		server.start();
 		server.join();
+
 	}
 }
