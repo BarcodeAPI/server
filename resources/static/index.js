265a581813a8276cc61221a2cd39b0735de39cde
Daniel Jones <dlucienjones@gmail.com>
2020-07-31 17:07:39 +0700

    feat: generate barcode type buttons from /types and add tooltips

--- resources/static/index.js
+++ resources/static/index.js
@@ -44,6 +44,23 @@ function closeMenu() {
 	const menu = document.getElementById("topnav");
 }
 
+function createBarcodeTypes(types) {
+    const menu = document.getElementById("topnav");
+    console.log(types[0].description)
+    for(t in types) {
+        var node = document.createElement("a");
+        node.setAttribute('id', "type-"+types[t].target);
+        node.setAttribute('rel', 'tooltip');
+        node.classList.add('top');
+        node.innerHTML = types[t].name;
+        node.setAttribute('onclick', 'setType("'+types[t].target+'")');
+        node.setAttribute('title', types[t].description);
+      //  node.setAttribute('onmouseover', 'showCodeDescription("'+types[t].target+'")');
+        menu.appendChild(node);
+    }
+    addTooltips();
+}
+
 /**
  * Called each time we should generate a new barcode.
  * 
@@ -189,7 +206,9 @@ function getCode(code) {
 
 function showCodeDescription(code) {
 
-	 console.log(getCode(code))
+    var barcode = getCode(code)
+	 console.log(barcode)
+	 console.log(barcode.description)
 }
 
 async function setPattern(hash) {
@@ -209,6 +228,8 @@ async function init() {
 	types = await getTypes();
 	hash = location.hash.substring(1);
 	await setPattern(hash);
+
+	createBarcodeTypes(types);
 
 	// hide copy image button in FF
 	if (sUsrAg.indexOf("Firefox") > -1) {
@@ -238,3 +259,95 @@ function checkIfFileSelected() {
 		}
 	});
 }
+
+
+function addTooltips() {
+
+      function getOffset(elem) {
+        var offsetLeft = 0, offsetTop = 0;
+        do {
+          if ( !isNaN( elem.offsetLeft ) )
+          {
+            offsetLeft += elem.offsetLeft;
+            offsetTop += elem.offsetTop;
+          }
+        } while( elem = elem.offsetParent );
+        return {left: offsetLeft, top: offsetTop};
+      }
+
+      var targets = document.querySelectorAll( '[rel=tooltip]' ),
+      target  = false,
+      tooltip = false,
+      title   = false,
+      tip     = false;
+
+      for(var i = 0; i < targets.length; i++) {
+        targets[i].addEventListener("mouseenter", function() {
+          target  = this;
+          tip     = target.getAttribute("title");
+          tooltip = document.createElement("div");
+          tooltip.id = "tooltip";
+
+          if(!tip || tip == "")
+          return false;
+
+          target.removeAttribute("title");
+          tooltip.style.opacity = 0;
+          tooltip.innerHTML = tip;
+          document.body.appendChild(tooltip);
+
+          var init_tooltip = function()
+          {
+            console.log(getOffset(target));
+            // set width of tooltip to half of window width
+            if(window.innerWidth < tooltip.offsetWidth * 1.5)
+            tooltip.style.maxWidth = window.innerWidth / 2;
+            else
+            tooltip.style.maxWidth = 340;
+
+            var pos_left = getOffset(target).left + (target.offsetWidth / 2) - (tooltip.offsetWidth / 2),
+            pos_top  = getOffset(target).top - tooltip.offsetHeight - 10;
+            console.log("top is", pos_top);
+            if( pos_left < 0 )
+            {
+              pos_left = getOffset(target).left + target.offsetWidth / 2 - 20;
+              tooltip.classList.add("left");
+            }
+            else
+            tooltip.classList.remove("left");
+
+            if( pos_left + tooltip.offsetWidth > window.innerWidth )
+            {
+              pos_left = getOffset(target).left - tooltip.offsetWidth + target.offsetWidth / 2 + 20;
+              tooltip.classList.add("right");
+            }
+            else
+            tooltip.classList.remove("right");
+
+            if( pos_top < 0 )
+            {
+              var pos_top  = getOffset(target).top + target.offsetHeight + 15;
+              tooltip.classList.add("top");
+            }
+            else
+            tooltip.classList.remove("top");
+            // adding "px" is very important
+            tooltip.style.left = pos_left + "px";
+            tooltip.style.top = pos_top + "px";
+            tooltip.style.opacity  = 1;
+          };
+
+          init_tooltip();
+          window.addEventListener("resize", init_tooltip);
+
+          var remove_tooltip = function() {
+            tooltip.style.opacity  = 0;
+            document.querySelector("#tooltip") && document.body.removeChild(document.querySelector("#tooltip"));
+            target.setAttribute("title", tip );
+          };
+
+          target.addEventListener("mouseleave", remove_tooltip );
+          tooltip.addEventListener("click", remove_tooltip );
+        });
+      }
+    }
