e5f0e543e32977b3cba71d6a4147d48cc158ec99
Matt Clark <matt@mclarkdev.com>
2026-02-21 11:33:34 -0500

    Adding support for Code 93.

---
+++ src/main/java/org/barcodeapi/server/gen/types/Code93Generator.java
@@ -0,0 +1,21 @@
+package org.barcodeapi.server.gen.types;
+
+import org.barcodeapi.server.core.CodeType;
+import org.barcodeapi.server.gen.impl.DefaultZXingProvider;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.oned.Code93Writer;
+
+/**
+ * QRCodeGenerator.java
+ * 
+ * @author Matthew R. Clark (BarcodeAPI.org, 2017-2026)
+ */
+public class Code93Generator extends DefaultZXingProvider {
+
+	public Code93Generator(CodeType codeType) {
+
+		// Setup QR generator
+		super(codeType, BarcodeFormat.CODE_93, new Code93Writer());
+	}
+}
